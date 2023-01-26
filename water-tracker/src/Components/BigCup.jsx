548f14c75ad78210e7589b0f767933a260e44c8b
Ece Çakmak <86318154+ececmk@users.noreply.github.com>
2023-01-26 16:26:21 +0100

    percentage css done

--- water-tracker/src/Components/BigCup.jsx
+++ water-tracker/src/Components/BigCup.jsx
@@ -1,29 +1,26 @@
-import React, {useRef, useEffect} from 'react';
+import React, { useRef, useEffect } from 'react';
 import PropTypes from 'prop-types'
 
 function BigCup(props) {
-  
-  const percentageRef = useRef(null);
 
-  useEffect(()=>{
-    if(props.currentValue === 0){
+  const percentageRef = useRef(null);
+  let percentageNumber = props.currentValue / props.goal;
+
+  useEffect(() => {
+    if (props.currentValue === 0) {
       percentageRef.current.style.visibility = 'hidden'
       percentageRef.current.style.height = 0
-    }else {
+    } else {
       percentageRef.current.style.visibility = 'visible'
-      percentageRef.current.style.height = `${props.currentValue/ props.goal * 330}px`
-      percentageRef.current.innerText = `${props.currentValue / props.goal * 100}%`
+      percentageRef.current.style.height = `${Math.round(percentageNumber * 330)}px`
+      percentageRef.current.innerText = `${Math.round(percentageNumber * 100)}%`
     }
-  }, [props.currentValue, props.goal])
+  }, [props.currentValue, props.goal, percentageNumber])
 
   let percentage = parseInt(props.currentValue / props.goal * 100 || 0, 10)
-  console.log('percentage', percentage);
 
-  console.log('goal', props.goal)
-  console.log('current', props.currentValue)
- 
   return (
-    <div className={`cup ${props.currentValue === 0 ? 'visible' : '' ||  props.currentValue === props.goal ? 'hidden!' : ''}`}>
+    <div className='cup'>
       <div ref={percentageRef} className="percentage">{percentage}%</div>
     </div>
   )
